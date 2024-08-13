57bd958efdda9e8812879f4182889ebcf1425b8b
Alvin Volok <97998961+SwiftAce1@users.noreply.github.com>
2024-08-13 14:15:15 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// script.js
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function calculateResult() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        display.value = 'Error';
+    }
+}
